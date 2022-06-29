86022680daa93e76b6d87bc28b7362efb43fb2b6
nahlavalentina <nahlavalentinas@gmail.com>
2022-06-29 08:07:42 -0300

    add router pharmacist

--- src/routes/App.routes.js
+++ src/routes/App.routes.js
@@ -1,10 +1,14 @@
 const express = require ("express");
 const ProductController = require("../controller/ProductController");
+const pharmacistController = require("../controller/pharmacistController")
 const router = express.Router();
 
 
-// Routes the of Product
+// Routes of Product
 router.route("/").get(ProductController.allProduct).post(ProductController.createProduct);
 router.route("/:id").get(ProductController.oneProduct).put(ProductController.UpdateProduct).delete(ProductController.deleteProduct)
+
+// Routs of Pharmacist
+router.route("/").get(pharmacistController.allPharmacist).post(pharmacistController.createPharmacist);
 
 module.exports = router;
