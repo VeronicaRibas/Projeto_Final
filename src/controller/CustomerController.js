4afbc28295e3d08ba334c01babd8ae8de3e84d40
Ana-Luiza-Ferreira <analuizaferreira105@gmail.com>
2022-06-30 16:38:08 -0300

    create route Customer

---
+++ src/controller/CustomerController.js
@@ -0,0 +1,13 @@
+const Supplier = require("../models/CustomerModel");
+
+module.exports = {
+    async createCustomer(request, response) {
+        try {
+            await Customer.create(request.body);
+            response.status(200).json("Customer successfully inserted");
+        } catch (error) {
+            response.status(400).send(error);
+        }
+    }
+
+}
--- src/routes/App.routes.js
+++ src/routes/App.routes.js
@@ -1,10 +1,15 @@
 const express = require ("express");
 const ProductController = require("../controller/ProductController");
+const CustomerController = require("../controller/CustomerController")
 const router = express.Router();
 
 
 // Routes the of Product
 router.route("/").get(ProductController.allProduct).post(ProductController.createProduct);
 router.route("/:id").get(ProductController.oneProduct).put(ProductController.UpdateProduct).delete(ProductController.deleteProduct)
+
+// Routes of Customer
+router.route("/").get(CustomerController.allCustomer).post(CustomerController.createCustomer);
+
 
 module.exports = router;
